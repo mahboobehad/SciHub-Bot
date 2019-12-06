2354b6cb045c5a3f437f20d2e663f312ce012932
mahboobe <mhstar32@gmail.com>
2019-12-06 11:49:29 +0330

    fix: Fixed typo.

--- view/sci_hub_view.py
+++ view/sci_hub_view.py
@@ -5,9 +5,8 @@ from view.button import Button
 
 class SciHubView:
     start_text = ("Send me a *science direct* url, "
-                  "I will fetch the paper and upload it for you here, otherwise I won't response :D")
-    add_cat = "Do you want to add category for downloaded paper?"
-
+                  "I will fetch the paper and upload it for you here, otherwise I won't respond :D")
+    
     enter_cat = "Enter category:"
 
     cat_added = "#{category} added to {title}"
@@ -22,9 +21,7 @@ class SciHubView:
     def send_paper(self, chat_id, file, title):
         keyboard = [[Button.add_category]]
         markup_keyboard = ReplyKeyboardMarkup(keyboard)
-        text = self.add_cat
-        self.bot.send_document(chat_id, document=file, caption=title)
-        self.bot.send_message(chat_id, text, markup_keyboard=markup_keyboard)
+        self.bot.send_document(chat_id, document=file, caption=title, reply_markup=markup_keyboard)
 
     def send_add_category(self, chat_id):
         text = self.enter_cat
